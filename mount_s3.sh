d13ba19509893a6c2b4fbab32627d0a8c55e8f10
Yangjianxiao0203 <yangjx@bu.edu>
2023-11-01 22:36:36 +0000

    revise bash

--- mount_s3.sh
+++ mount_s3.sh
@@ -1,3 +1,10 @@
+sudo apt install awscli
+sudo apt install s3fs
+
+chmod 600 bucket_pass
+
+mkdir s3
+
 sudo s3fs deeplearningbucket ./s3 -o passwd_file=$(pwd)/bucket_pass -o allow_other
 
 sudo usermod -a -G root ubuntu
--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 #conda
-conda create --name nlp python=3.10
+# conda create --name nlp python=3.10
 conda activate nlp
 
 # Install the requirements
 pip install -r requirements.txt
 
 # Install Git LFS
-git lfs install
+# git lfs install
 
 # Clone the repository
-git clone https://huggingface.co/bert-base-uncased
+# git clone https://huggingface.co/bert-base-uncased
 
 # Run the loader.py to get data from the dataset
 python loader.py
